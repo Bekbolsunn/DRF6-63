927678b1256af79092d51859eb3b237b8710546c
Bekbolsunn <karataevbekbolsun@gmail.com>
2026-05-26 21:00:09 +0600

    lesson1, CustomUser

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
+from users.models import CustomUser
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    list_display = ["id", "email", "is_active"]
+    fieldsets = (
+        (None, {"fields": ("email", "password", "is_active")}),
+        ("Important dates", {"fields": ("last_login",)}),
+    )
+    ordering = ["email"]
